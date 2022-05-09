8fa023f00b4a163d27592279dd1c76ab86114e8c
RedaBoi <redabusiness10@gmail.com>
2022-05-09 17:00:10 +0100

    Heroku implementation

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/kingo-news'));
+app.use(express.static('./dist/moroccan-news'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/kingo-news/' }),
+    res.sendFile('index.html', { root: 'dist/moroccan-news/' }),
 );
 
 // Start the app by listening on the default Heroku port
